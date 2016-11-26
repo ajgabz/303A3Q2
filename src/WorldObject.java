8b0e18f3fd454df82ea21acdbaf94bca2284938e
Aaron Gaba <airplane679@gmail.com>
2016-11-26 13:25:24 -0500

    Constructor, getters & setters added for fields of token and name

--- src/WorldObject.java
+++ src/WorldObject.java
@@ -4,4 +4,36 @@ public abstract class WorldObject {
 	private char token;
 	private String name;
 	
+	public WorldObject(char objectToken, String objectName) {
+		this.token = objectToken;
+		this.name = objectName;
+	}
+	
+	
+	/**
+	 * @return the token
+	 */
+	public char getToken() {
+		return token;
+	}
+	/**
+	 * @param token the token to set
+	 */
+	public void setToken(char token) {
+		this.token = token;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
